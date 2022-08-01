85d93ea27dd820f3ae42f770ce68ddb1f99539c1
Nathan Anderson <nathan.anderson.1024@gmail.com>
2022-08-01 11:52:18 -0700

    added styling for chat on 4k

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ function addMessage(userMessage) {
     Data.push(userMessage); //Adds users text to the array
     const li = document.createElement('li'); //creates a new list item 
     li.classList.add('hover-rows'); //adds hover-rows class to li
+    li.classList.add('chat-item') //adds chat-item class to li
     li.innerHTML = userMessage; //sets li in the html
     chatHistory.appendChild(li); //appends li to the ul
     localStorage.setItem('chat', JSON.stringify(Data)); //saves entry in local storage
