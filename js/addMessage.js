f95905cc3b5b3af2a6ef4f7de0cb7ebef244768c
Nathan Anderson <nathan.anderson.1024@gmail.com>
2022-09-14 10:44:40 -0700

    refactor addMessage.js

--- js/addMessage.js
+++ js/addMessage.js
@@ -6,21 +6,29 @@ export const Data = []
 export function addMessage(userMessage) {
     const htmlInjectTest = (new RegExp(/</g)).test(userMessage)
     if (htmlInjectTest) {
-        const sanitizedMessage = userMessage.replace(/</g,"&lt;");
-        Data.push(sanitizedMessage); //Adds users text to the array
-        const li = document.createElement('li'); //creates a new list item 
-        li.classList.add('hover-rows','chat-item'); //adds hover-rows class to li
-        li.innerHTML = sanitizedMessage; //sets li in the html
-        chatHistory.appendChild(li); //appends li to the ul
-        localStorage.setItem('chat', JSON.stringify(Data)); //saves entry in local storage
+        santizeMessage(userMessage);
     }
     else {
-        Data.push(userMessage); //Adds users text to the array
-        const li = document.createElement('li'); //creates a new list item 
-        li.classList.add('hover-rows','chat-item'); //adds hover-rows class to li
-        li.innerHTML = userMessage; //sets li in the html
-        chatHistory.appendChild(li); //appends li to the ul
-        localStorage.setItem('chat', JSON.stringify(Data)); //saves entry in local storage
+        defaultMessage(userMessage)
     }
     
+}
+
+function santizeMessage(userMessage) {
+    const sanitizedMessage = userMessage.replace(/</g,"&lt;");
+    Data.push(sanitizedMessage); //Adds users text to the array
+    const li = document.createElement('li'); //creates a new list item 
+    li.classList.add('hover-rows','chat-item'); //adds hover-rows class to li
+    li.innerHTML = sanitizedMessage; //sets li in the html
+    chatHistory.appendChild(li); //appends li to the ul
+    localStorage.setItem('chat', JSON.stringify(Data)); //saves entry in local storage
+}
+
+function defaultMessage(userMessage) {
+    Data.push(userMessage); //Adds users text to the array
+    const li = document.createElement('li'); //creates a new list item 
+    li.classList.add('hover-rows','chat-item'); //adds hover-rows class to li
+    li.innerHTML = userMessage; //sets li in the html
+    chatHistory.appendChild(li); //appends li to the ul
+    localStorage.setItem('chat', JSON.stringify(Data)); //saves entry in local storage
 }
